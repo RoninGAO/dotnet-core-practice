296051ea0581e64450d75a673c96dd2bdd8d1ac1
RoninGAO <minkou.lee@gmail.com>
2018-08-09 17:11:42 +0800

    repo init

---
+++ 00_EnumToString/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace EnumToString
+{
+    class Program
+    {
+        enum Colors { Red = 0, Green = 1, Blue = 2, Yellow = 12 };
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Colors.Red = {0}", Colors.Red);
+            Console.WriteLine("Colors.Red.ToString() = {0}", Colors.Red.ToString());
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("Colors.Blue.ToString(\"d\") = {0}", Colors.Red.ToString("d"));
+            Console.WriteLine("Colors.Blue.ToString(\"D\") = {0}", Colors.Red.ToString("D"));
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("Colors.Blue.ToString(\"g\") = {0}", Colors.Red.ToString("g"));
+            Console.WriteLine("Colors.Blue.ToString(\"G\") = {0}", Colors.Red.ToString("G"));
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("///////////////////////////");
+            Console.WriteLine("Enum.ToString() = Enum.ToString(\"g\")");
+            Console.WriteLine("///////////////////////////");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,35 @@
+# dotnet-core-practice
+
+> As title.
+
+## Getting Started
+
+>These instructions will get you a copy of the project up and running on your local >machine for development and testing purposes. See deployment for notes on how to >deploy the project on a live system.
+
+
+## Prerequisites
+
+this practice need to install the software list
+
+1. .Net Core
+
+
+## Versioning
+
+We use [SemVer](http://semver.org/) for versioning. For the versions available, see the [tags on this repository](https://github.com/RoninGAO/react-with-redux-app-practice/tags).
+
+## Authors
+
+* **SAM LEE** - *Initial work* - [RoninGAO](https://github.com/RoninGAO)
+
+See also the list of [contributors](https://github.com/RoninGAO/react-with-redux-app-practice/contributors) who participated in this project.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgments
+
+* Hat tip to anyone whose code was used
+* Inspiration
+* etc
